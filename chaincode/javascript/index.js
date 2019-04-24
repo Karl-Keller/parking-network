acd5d2c6af30373bb08afb420f9adbe9e7bfc859
Keller <kkeller2@jmt.com>
2019-04-24 14:20:11 -0400

    Adding the javascript chaincode rigging

---
+++ chaincode/javascript/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const Parking = require('./lib/parking');
+
+module.exports.parking = Parking;
+module.exports.contracts = [ Parking ];
